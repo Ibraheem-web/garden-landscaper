ce0ee2bcb602850e1eebe7254e206897103c2dfa
muhammad-ibraheem <mibraheem33532233@gmail.com>
2023-04-10 12:29:33 +0500

    add files

---
+++ script.js
@@ -0,0 +1,21 @@
+const mainMenu = document.querySelector('.mainMenu');
+const openMenu = document.querySelector('.openMenu');
+const closeMenu = document.querySelector('.closeMenu');
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+
+function show() {
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+    openMenu.style.display = 'none';
+    closeMenu.style.display = 'flex';
+}
+
+
+function close() {
+    mainMenu.style.top = '-1000%';
+    closeMenu.style.display = 'none';
+    openMenu.style.display = 'flex';
+}
